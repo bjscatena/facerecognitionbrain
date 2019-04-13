07e67714da07aaac6d6228472953bbfe559ee178
bjscatena <brunojsxc@gmail.com>
2019-04-13 15:54:30 -0300

    Updates

---
+++ precache-manifest.79a2bd9a22fab8722048b35ecae52e9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce2d7a8efc8c8d84e679",
+    "url": "/facerecognitionbrain/static/js/runtime~main.2d683c34.js"
+  },
+  {
+    "revision": "d9d77ee61f372f02b8c1",
+    "url": "/facerecognitionbrain/static/js/main.a43b95e2.chunk.js"
+  },
+  {
+    "revision": "f867ec5ebceb392467f4",
+    "url": "/facerecognitionbrain/static/js/2.e604db7f.chunk.js"
+  },
+  {
+    "revision": "d9d77ee61f372f02b8c1",
+    "url": "/facerecognitionbrain/static/css/main.17a55e65.chunk.css"
+  },
+  {
+    "revision": "f867ec5ebceb392467f4",
+    "url": "/facerecognitionbrain/static/css/2.1cf1d2d5.chunk.css"
+  },
+  {
+    "revision": "c7b8a665194e6d53f8a377d5c326621b",
+    "url": "/facerecognitionbrain/index.html"
+  }
+];
